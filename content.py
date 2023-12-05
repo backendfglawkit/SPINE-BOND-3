e5f1a408e51a2c33579fd02dc45f358b2abb7ddf
AimTheCreator <137602587+AimTheCreator@users.noreply.github.com>
2023-12-05 20:39:05 +0530

    first commit

---
+++ content.py
@@ -0,0 +1,75 @@
+##############################
+##      home page data      ##
+##############################
+
+
+# {s,s_h,c}
+
+feature=[
+         {'h':'Personalized Comfort','s_h':"Your Perfect Night's Sleep",'c':"At Spine-Bond, we understand that everyone has their unique sleep preferences. That's why our products are designed for personalized comfort, ensuring you get the sleep experience tailored to your needs."},
+         {'h':'Orthopedic Excellence','s_h':'Experience the Science of Sleep','c':"Our mattresses and pillows are engineered with precision to provide exceptional orthopedic support. Say goodbye to waking up with aches and pains. With Spine-Bond, you'll enjoy nights of deep, restorative sleep that leave you feeling revitalized and ready for the day ahead."},
+         {'h':'Premium Materials','s_h':'Indulge in Luxury Comfort','c':"We source only the finest materials to create our products. From high-density memory foam to premium fabric covers, every Spine-Bond item exudes quality and comfort. You'll feel the difference from the moment you lay down."},
+        #  {'h':'Temperature-Regulating Innovation' ,'s-h':"Stay Cool, Sleep Better" ,'c' : "Experience cutting-edge temperature-regulating technology in our mattress covers. Say goodbye to overheating during the night and wake up feeling refreshed, thanks to Spine-Bond's innovative cooling solutions."}
+         ]
+
+product_mat=[
+        {'name':'Health Care',
+         'images':['hc1.jpg','hc2.jpg'],
+         'feature':['Good','Nice','ghj','trr'],
+         'dics':'This exquisitely designed mattress features a reinforced bonded foam core and optimal cushioning, ensuring exceptional support for every contour of your body, promoting a truly restful sleep experience. It is elegantly quilted with high-density bonded and EPE foam, all encased in a vibrant, colorful printed fine cotton tapestry.This cost-effective collection is offered in a 4-inch thickness variant, catering to your comfort needs with style and affordability.',
+         'size':[
+                    {'warranty':'2','height':'4','width':{'72x30':'3761','72x36':'4497','72*48':'6224','72x60':'7521','72x72':'8993','75x30':'4508','75x36':'5277','75x48':'7035','75x60':'8671','75x72':'10554'}}
+                ]
+        },
+        
+        {'name':'Silver',
+         'images':['s1.jpg','s2.jpg'],
+         'feature':['asd','ert','ghj','trr'],
+         'dics':'This innovative creation is suitable for all age groups and segments, delivering impeccable spine support and serving as an effective solution for back pain relief, surpassing the existing standards of other brands. It combines an extra-dense bonded foam core with an 8mm superior-grade high-density foam comfort layer, all elegantly quilted with eye-catching, cotton-satin printed fine tapestry in an array of attractive colors. This mattress offers superior comfort at an affordable price and comes in a thickness of 4+ inches.',
+         'size':[
+                    {'warranty':'2','height':'4','width':{'72x30':'4701','72x36':'5621','72x48':'7780','72x60':'9401','72x72':'11241','75x30':'5635','75x36':'6596','75x48':'8793','75x60':'10839','75x72':'13192','78x30':'5859','78x36':'6860','78x48':'9146','78x60':'11719','78x72':'13718'}},
+                    {'warranty':'3','height':'5','width':{'72x30':'5630','72x36':'6757','72x48':'9248','72x60':'10342','72x72':'13516','75x30':'6199','75x36':'7257','75x48':'9693','75x60':'11923','75x72':'14511','78x30':'6446','78x36':'7546','78x48':'10060','78x60':'12890','78x72':'15090'}},
+                    {'warranty':'5','height':'6','width':{'72x30':'6148','72x36':'5621','72x48':'10564','72x60':'11376','72x72':'15902','75x30':'6818','75x36':'7987','75x48':'10640','75x60':'13115','75x72':'15962','78x30':'7090','78x36':'8301','78x48':'11066','78x60':'14180','78x72':'16599'}}
+                ]
+        },
+        {'name':'Gold',
+         'images':['g1.jpg','g2.jpg','g3.jpg','g4.jpg'],
+         'feature':['asd','ert','ghj','trr'],
+         'dics':'Introducing the Perfect Mattress, a harmonious blend of luxury and orthopaedic excellence. Ideal for those seeking optimal support for the Back, Hip, and Neck regions, this mattress is a top choice for individuals dealing with back and joint pains or those aiming to proactively prevent them. The Extra Densified & Reinforced Bonded Foam Core, enveloped in Luxurious Glazed Cotton Printed Fabric, offers both durability and aesthetics. With Hi Loft Quilting and Super Soft Hi-Density Foam, this mattress ensures a soothing comfort that promotes restful sleep. Boasting a Thickness of 4.5 inches and compatibility with a 5-inch frame, the Perfect Mattress caters to diverse preferences, promising a rejuvenating sleep experience for all.',
+         'size':[
+                    {'warranty':'3','height':'4','width':{'72x30':'5947','72x36':'6959','72x48':'9333','72x60':'11117','72x72':'13141','75x30':'6974','75x36':'8032','75x48':'10448','75x60':'12698','75x72':'15287','78x30':'7221','78x36':'8321','78x48':'10835','78x60':'13665','78x72':'15865'}},
+                    {'warranty':'5','height':'5','width':{'72x30':'6489','72x36':'7843','72x48':'10151','72x60':'12979','72x72':'15226','75x30':'6760','75x36':'8112','75x48':'10816','75x60':'13519','75x72':'16224','78x30':'7030','78x36':'8436','78x48':'11248','78x60':'14061','78x72':'16873'}},
+                    {'warranty':'7','height':'6','width':{'72x30':'7146','72x36':'8985','72x48':'11329','72x60':'14292','72x72':'17970','75x30':'7444','75x36':'8933','75x48':'11911','75x60':'14888','75x72':'18440','78x30':'7742','78x36':'9290','78x48':'12387','78x60':'15484','78x72':'19729'}}
+                ]
+        },
+        {'name':'Diamond',
+         'images':['d1.jpg','d2.jpg','d3.jpg','d4.jpg'],
+         'feature':['asd','ert','ghj','trr'],
+         'dics':'Introducing the Perfect Mattress, a harmonious blend of luxury and orthopaedic excellence. Ideal for those seeking optimal support for the Back, Hip, and Neck regions, this mattress is a top choice for individuals dealing with back and joint pains or those aiming to proactively prevent them. The Extra Densified & Reinforced Bonded Foam Core, enveloped in Luxurious Glazed Cotton Printed Fabric, offers both durability and aesthetics. With Hi Loft Quilting and Super Soft Hi-Density Foam, this mattress ensures a soothing comfort that promotes restful sleep. Boasting a Thickness of 4.5 inches and compatibility with a 5-inch frame, the Perfect Mattress caters to diverse preferences, promising a rejuvenating sleep experience for all.',
+         'size':[
+                    {'warranty':'5','height':'4','width':{'72x30':'6739','72x36':'7491','72x48':'9988','72x60':'13478','72x72':'14982','75x30':'7020','75x36':'8424','75x48':'11232','75x60':'14039','75x72':'16847','78x30':'7300','78x36':'8760','78x48':'11680','78x60':'14602','78x72':'17521'}},
+                    {'warranty':'8','height':'5','width':{'72x30':'7377','72x36':'8441','72x48':'11255','72x60':'14755','72x72':'16882','75x30':'7684','75x36':'9222','75x48':'12296','75x60':'15370','75x72':'18444','78x30':'7993','78x36':'9591','78x48':'12787','78x60':'15984','78x72':'19181'}},
+                    {'warranty':'10','height':'6','width':{'72x30':'8484','72x36':'9707','72x48':'12943','72x60':'16968','72x72':'19414','75x30':'8838','75x36':'10605','75x48':'14139','75x60':'17674','75x72':'21209','78x30':'9191','78x36':'11029','78x48':'14705','78x60':'18382','78x72':'22058'}}
+                ]
+        },
+        {'name':'Kohinoor',
+         'images':['K1.jpg','K2.jpg','K3.jpg'],
+         'feature':['asd','ert','ghj','trr'],
+         'dics':'Introducing the Perfect Mattress, a harmonious blend of luxury and orthopaedic excellence. Ideal for those seeking optimal support for the Back, Hip, and Neck regions, this mattress is a top choice for individuals dealing with back and joint pains or those aiming to proactively prevent them. The Extra Densified & Reinforced Bonded Foam Core, enveloped in Luxurious Glazed Cotton Printed Fabric, offers both durability and aesthetics. With Hi Loft Quilting and Super Soft Hi-Density Foam, this mattress ensures a soothing comfort that promotes restful sleep. Boasting a Thickness of 4.5 inches and compatibility with a 5-inch frame, the Perfect Mattress caters to diverse preferences, promising a rejuvenating sleep experience for all.',
+         'size':[
+                    {'warranty':'10','height':'Euro Top - 6','width':{'72x30':'9021','72x36':'10428','72x48':'13905','72x60':'18043','72x72':'20857','75x30':'9397','75x36':'11276','75x48':'15036','75x60':'18794','75x72':'22554'}},
+                    {'warranty':'12','height':'Pillow Top - 6','width':{'72x30':'10554','72x36':'12200','72x48':'16266','72x60':'21107','72x72':'24399','75x30':'10993','75x36':'13191','75x48':'17589','75x60':'21986','75x72':'26384'}}
+                ]
+        },
+        
+    ]
+
+product_pillow= [{'image':["1.jpg"],'name':'12x12 Car Cushion'},
+                 {'image':["2.jpg"],'name':'16x16 Cushion Pillow'},
+                 {'image':["3.jpg"],'name':'21x21 Sofa Cushion Pillow'},
+                 {'image':["4.jpg"],'name':'16x24 Bed Pillow Medium'},
+                 {'image':["5.jpg"],'name':'17x27 Bed Pillow Large'},
+                 {'image':["6.jpg"],'name':'24x24 Bolister'}]
+
+product_protecor=[{'name':'Cotton Protector','image':'pro1.jpg'},
+                  {'name':'Polyester  Protector','image':'pro2.jpg'}]
---
+++ main.py
@@ -0,0 +1,46 @@
+from flask import Flask, render_template
+import content
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html', feature_data=content.feature)
+
+@app.route('/about-us')
+def about():
+    return 'this is home route'
+@app.route('/product')
+def product():
+    return render_template('all_product.html')
+
+@app.route('/product/<value>')
+def product_inner(value):
+    if value== 'mattress':
+        return render_template('product.html',product_det=content.product_mat)
+    elif value == 'pillow':
+        return render_template ('pillow.html',product_det=content.product_pillow)
+    elif value == 'protector':
+        return render_template ('Protector.html',product_det=content.product_protecor)
+
+@app.route('/product/<value>/<value1>')
+def product_det(value,value1):
+    if value == "mattress":
+        for i in content.product_mat:
+            if i["name"] == value1 :
+                data = {'name':i['name'],'dics':i['dics'],'images':i['images']}
+                size=i['size']
+    return render_template ('pro_det.html',data=data,size=size)
+
+@app.route('/product/<value>/<value1>/<value2>')
+def product_det_size(value,value1,value2):
+    if value == "mattress":
+        value2=(int(value2)-1)
+        for i in content.product_mat:
+            if i["name"] == value1 :
+                data = {'name':i['name'],'dics':i['dics'],'size':i['size'][value2],'images':i['images']}
+                size=i['size']
+    return render_template ('pro_det.html',data=data,size=size)
+
+if __name__ == '__main__':
+    app.run(debug=True)
